b24bebf1240a62f51b5b9fd35197da90eb6ba55c
Lynn Port <Lport@ewu.edu>
2026-01-12 16:29:45 +0000

    initialized content to 0-1-codex-console.js

---
+++ scripts/week 0/0-1-codex-console.js
@@ -0,0 +1,62 @@
+// ============================================
+// TUTORIAL 0-1: CODEDEX CONSOLE FUNDAMENTALS
+// Student: Lynn Port
+// Date: 1/11/26
+// ============================================
+
+// --------------------------------------------
+// EXERCISE 1: SETTING UP
+// --------------------------------------------
+console.log("Hello internet!");
+// Add your custom greeting below:
+console.log("Hello internet!");
+
+// --------------------------------------------
+// EXERCISE 2: THE CONSOLE
+// Your favorite food
+// --------------------------------------------
+console.log("Apple")
+//For some reason, Apple is the only accepted answer on the site. Wish the instructions would have stated that. 
+
+// --------------------------------------------
+// EXERCISE 3: LETTER TREE
+// The pattern: a, b c, d e f, g h i j, k
+// --------------------------------------------
+console.log("   a");
+console.log("  b c");
+console.log(" d e f");
+console.log("g h i j");
+console.log("   k");
+
+// --------------------------------------------
+// EXERCISE 4: SECRET RECIPE
+// Use comments to document your recipe
+// Include a "secret ingredient" comment
+// --------------------------------------------
+console.log("Apple pie recipe");
+console.log("Ingredients: 5 apples, 1/2 cup sugar, 1 stick cinnamon, premade pie crust");
+// secret ingredient: 1/2 teaspoon cardamom
+console.log("Directions:");
+console.log("Step 1. cut apples into thin slices, set in bowl");
+console.log("Step 2. grate cinnamon into apples");
+console.log("Step 3. add sugar to apples, mix");
+console.log("Step 4. add all to pie crust, bake");
+//idk I don't have a secret recipie lol
+
+// --------------------------------------------
+// EXERCISE 5: RECEIPT
+// Format a receipt from a recent purchase
+// --------------------------------------------
+console.log("==================================");
+console.log("         McDONALD'S RECEIPT");
+console.log("----------------------------------");
+console.log("           Order Details");
+console.log("----------------------------------");
+console.log("Item           Quantity   Price");
+console.log("----------------------------------");
+console.log("🍔 Burger       1          2.99");
+console.log("🍟 Fries        1          2.49");
+console.log("🥤 Soda         1          1.99");
+console.log("----------------------------------");
+console.log("Total                      7.47");
+console.log("==================================");
