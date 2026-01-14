c00579824cac5fb97d7a694587f3dc6cd876e9e6
Lynn Port <Lport@ewu.edu>
2026-01-14 22:46:58 +0000

    added functionality to the change user button

--- scripts/week 0/behavioral-layer/beginner-html-site-styled-main/main.js
+++ scripts/week 0/behavioral-layer/beginner-html-site-styled-main/main.js
@@ -13,3 +13,19 @@ myImage.addEventListener("click", () => {
 === is a strict equality operator, which according to google means It checks if two operands are equal in both value and type, without performing any type conversion
 an operand is a value, variable, or expression that an operator acts upon to produce a result
 */
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
