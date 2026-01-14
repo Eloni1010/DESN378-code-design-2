24380d5b7e52b0b64b7a5f8fca17bcc0a0f65c61
Lynn Port <Lport@ewu.edu>
2026-01-14 22:49:31 +0000

    updated setUserName code

--- scripts/week 0/behavioral-layer/beginner-html-site-styled-main/main.js
+++ scripts/week 0/behavioral-layer/beginner-html-site-styled-main/main.js
@@ -17,8 +17,12 @@ let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 function setUserName() {
   const myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = `Mozilla is cool, ${myName}`;
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
 }
 if (!localStorage.getItem("name")) {
   setUserName();
