b883ecc4e767d58d8feb91b827a2dce61173513c
Lynn Port <Lport@ewu.edu>
2026-01-14 22:35:34 +0000

    added explanitory comments

--- scripts/week 0/behavioral-layer/beginner-html-site-styled-main/main.js
+++ scripts/week 0/behavioral-layer/beginner-html-site-styled-main/main.js
@@ -8,3 +8,8 @@ myImage.addEventListener("click", () => {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
 });
+/*An event listener looks out for when a specified event happens.
+=> is an arrow function, which according to google means that it takes two values and returns their sum
+=== is a strict equality operator, which according to google means It checks if two operands are equal in both value and type, without performing any type conversion
+an operand is a value, variable, or expression that an operator acts upon to produce a result
+*/
